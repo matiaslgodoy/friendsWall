f5326f9019a90d41b0c2325ed2587ce34d8a143c
matias <matias.godoy@darwoft.com>
2019-07-18 17:50:59 -0300

    modal

---
+++ src/app/interfaces/friend.ts
@@ -0,0 +1,8 @@
+export interface Friend {
+  name: string;
+  instagram?: string;
+  facebook?: string;
+  twitter?: string;
+  uid: any;
+  photo?: string;
+}
--- src/app/pages/friend-modal/friend-modal.component.ts
+++ src/app/pages/friend-modal/friend-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Friend} from '../../interfaces/friend';
 
 @Component({
   selector: 'app-friend-modal',
@@ -7,6 +8,9 @@ import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./friend-modal.component.css']
 })
 export class FriendModalComponent implements OnInit {
+  friend: Friend = <Friend>{};
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
 
   constructor(public activeModal: NgbActiveModal) { }
 
@@ -14,5 +18,17 @@ export class FriendModalComponent implements OnInit {
     console.log('MODAL!!!!');
   }
 
-  //funcionalidad de agregar
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+  imageCropped(image: string) {
+    this.croppedImage = image;
+  }
+  imageLoaded() {
+    // show cropper
+  }
+  loadImageFailed() {
+    // show message
+  }
+
 }
